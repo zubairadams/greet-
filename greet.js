f37a678dafbe4fd225e5f0aa07d406b6e93df5a3
zubairadams <zubair.adams720@gmail.com>
2018-06-14 10:01:30 +0200

    not yet

--- greet.js
+++ greet.js
@@ -1,38 +1,17 @@
 const languages = document.querySelector('.language');
-const buttons = document.querySelector('.buts')
+const button = document.querySelector('.buts')
 const reset = document.querySelector('.butts')
 var check = document.querySelector("input[name='language']:checked")
 var NameEntered = document.querySelector('.type')
 var greeted = document.querySelector('.answer')
 
+var factory = greeting();
 
+function greetingDom() {
 
-
-
-var factory = greeting()
-
-function greetDom() {
-  factory.greetEnglish();
-  factory.greetAfrikaan();
-  factory.greetArabic();
+factory.English();
+factory.Afrikaans();
+factory.Arabic();
 }
 
-function greet() {
-  //when the greet button is pressed check if this user was already greeted before
-  //by looking if the userName exists in namesGreeted if not increment this counter and update the screen
-  if (namesGreeted[NameEntered] === undefined) {
-    greetingsCounter++;
-    //add an entry for the user that was greeted in the Object Map
-    namesGreeted[NameEntered] = 0;
-    //update the DOM to display the counter
-    greeted.innerHTML = greetingsCounter;
-
-  }
-
- reset.addEventListener('click',function()){
-
- }
-
-  buttons.addEventListener('click', function()) {
-        greetDom
-      }
+button.addEventlistener()
